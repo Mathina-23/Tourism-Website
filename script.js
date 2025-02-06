0c3c59cf1d558ae9282ccd606f717bc9b71d1406
Mathina-23 <mathina23beevi@gmail.com>
2025-02-06 10:29:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function setNewImg2(){
+    document.getElementById('img-2').src="img/img-12.jpg";
+}
+function setOldImg12(){
+    document.getElementById('img-2').src="img/img-2.jpg";    
+}
+function setNewImg3(){
+    document.getElementById('img-3').src="img/img-13.jpg";
+}
+function setOldImg13(){
+    document.getElementById('img-3').src="img/img-3.jpg";    
+}
+function setNewImg4(){
+    document.getElementById('img-4').src="img/img-14.jpg";
+}
+function setOldImg14(){
+    document.getElementById('img-4').src="img/img-4.jpg";    
+}
+function setNewImg5(){
+    document.getElementById('img-5').src="img/img-15.jpg";
+}
+function setOldImg15(){
+    document.getElementById('img-5').src="img/img-5.jpg";    
+}
+function setNewImg6(){
+    document.getElementById('img-6').src="img/img-16.jpg";
+}
+function setOldImg16(){
+    document.getElementById('img-6').src="img/img-6.jpg";    
+}
+function setNewImg7(){
+    document.getElementById('img-7').src="img/img-17.jpg";
+}
+function setOldImg17(){
+    document.getElementById('img-7').src="img/img-7.jpg";    
+}
